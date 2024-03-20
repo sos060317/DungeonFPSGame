26fda2db8e6d42ae0c3c98389f3a2e0f71dbe33b
WooJiWon <woojiwon060317@gmail.com>
2024-03-20 20:53:29 +0900

    몬스터 절대 범위

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -1,31 +1,35 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(FieldOfView))] // FieldOfView에 대한 커스텀 에디터
+[CustomEditor(typeof(EnemyBase), true)] // FieldOfView에 대한 커스텀 에디터, 하위 클래스에도 적용
 public class FieldOfViewEditor : Editor
 {
     private void OnSceneGUI() // GUI 그려주는 함수
     {
-        FieldOfView fov = (FieldOfView)target; // 형변환
+        EnemyBase fov = (EnemyBase)target; // 형변환
         Handles.color = Color.white;           // 색상 변환(흰색)
         // 컴포넌트가 부착된 오브젝트 기준으로 원 모양의 와이어프레임을 그림
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.fieldOfView.lookRadius);
+
+        Handles.color = Color.red;           // 색상 변환(빨간색)
+        // 컴포넌트가 부착된 오브젝트 기준으로 원 모양의 와이어프레임을 그림
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.fieldOfView.noLookRadius);
 
         // 시야각 제한선의 방향을 저장
-        Vector3 viewAngle01 = DirectionFormAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
-        Vector3 viewAngle02 = DirectionFormAngle(fov.transform.eulerAngles.y, fov.angle / 2);
+        Vector3 viewAngle01 = DirectionFormAngle(fov.transform.eulerAngles.y, -fov.fieldOfView.angle / 2);
+        Vector3 viewAngle02 = DirectionFormAngle(fov.transform.eulerAngles.y, fov.fieldOfView.angle / 2);
 
         Handles.color = Color.yellow; // 색상 변환(노란색)
         // 플레이어의 위치에서 방향 * 반지름 만큼 선을 그음
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.radius);  
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.radius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.fieldOfView.lookRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.fieldOfView.lookRadius);
 
-        if (fov.canSeePlayer) // 플레이어가 시야각 안에 있다면
+        if (fov.fieldOfView.canSeePlayer) // 플레이어가 시야각 안에 있다면
         {
             Handles.color = Color.green; // 색상 변환(초록색)
 
             // 내 위치에서 플레이어의 위치로 선을 그림
-            Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+            Handles.DrawLine(fov.transform.position, fov.fieldOfView.playerRef.transform.position);
         }
     }
 
--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class EnemyBase : MonoBehaviour
 {
-    public float radius;                          // 시야 반경
-    [Range(0, 360)]
-    public float angle;                           // 시야각
+    [System.Serializable]
+    public struct FieldOfView
+    {
+        public float lookRadius;          // 시야각 안에 있어야 감지하는 시야 반경
+        public float noLookRadius;        // 일정 범위 내에 있으면 그냥 감지해버리는 시야 반경
+        [Range(0, 360)]                   
+        public float angle;               // 시야각
+                                          
+        [Space(10f)]                      
+        public LayerMask targetMask;      // 목표
+        public LayerMask obstructionMask; // 장애물(ex: 벽)
+                                          
+        [HideInInspector]                 
+        public bool canSeePlayer;         // 플레이어를 보고있는지 아닌지를 참거짓으로 확인
+                                          
+        [HideInInspector]                 
+        public GameObject playerRef;      // 플레이어 오브젝트
+    }
 
-    public GameObject playerRef;                  // 플레이어 오브젝트
+    [Header("Enemy Base")]
+    public FieldOfView fieldOfView;       // Field of View 구조체
 
-    [SerializeField]
-    private LayerMask targetMask, obstructionMask; // 목표, 장애물(ex: 벽)
-
-    public bool canSeePlayer;                     // 플레이어를 보고있는지 아닌지를 참거짓으로 확인
+    private NavMeshAgent agent;           // NavMeshAgent 컴포넌트
+    private Animator animator;            // Animator 컴포넌트
 
     protected virtual void Start()
     {
-        playerRef = GameObject.FindGameObjectWithTag("Player"); // Tag가 "Player"인 오브젝트로 변수 초기화
+        agent = GetComponent<NavMeshAgent>(); // NavMeshAgent 초기화
+        animator = GetComponent<Animator>();  // Animator     초기화
+
+        fieldOfView.playerRef = GameObject.FindGameObjectWithTag("Player"); // Tag가 "Player"인 오브젝트로 변수 초기화
+
         StartCoroutine(FOVRoutine()); // 코루틴 호출
     }
+
+    protected abstract void Move();
 
     private IEnumerator FOVRoutine()
     {
@@ -34,36 +55,50 @@ public abstract class EnemyBase : MonoBehaviour
     private void FieldOfViewCheck()
     {
         // radius값을 반지름으로 한 원 안에 특정 레이어(targetMask)를 가지고 있는 충돌체를 감지하여 배열에 저장함
-        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+        Collider[] wideRangeChecks   = Physics.OverlapSphere(transform.position, fieldOfView.lookRadius, fieldOfView.targetMask);
+        Collider[] narrowRangeChecks = Physics.OverlapSphere(transform.position, fieldOfView.noLookRadius, fieldOfView.targetMask);
 
-        if (rangeChecks.Length != 0)
+        if (narrowRangeChecks.Length != 0) // 좁은 범위에 충돌체가 있으면
         {
-            Transform target = rangeChecks[0].transform; // 첫번째로 충돌된 충돌체의 위치를 저장함
+            agent.isStopped = false;
+            Transform narrowTarget = narrowRangeChecks[0].transform; // 좁은 범위에 첫번째로 충돌된 충돌체의 위치를 저장함
+            fieldOfView.canSeePlayer = true;                         // 플레이어를 봄
+            animator.SetBool("canSeePlayer", true);                  // animator 상태 변경
+            agent.SetDestination(narrowTarget.position);             // wideTarget 따라가기
+        }
+        else if (wideRangeChecks.Length != 0) // 넓은 범위에 충돌체가 있으면
+        {
+            Transform wideTarget = wideRangeChecks[0].transform; // 넓은 범위에 첫번째로 충돌된 충돌체의 위치를 저장함
 
             // 두 벡터의 위치 값을 빼고 정규화를 통해서 방향 벡터를 1로 하는 단위 벡터를 저장함
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            Vector3 directionToTarget = (wideTarget.position - transform.position).normalized;
 
             // 내 전방과 directionToTarget사이의 값이 angle의 절반보다 작으면 if문 실행
-            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            if (Vector3.Angle(transform.forward, directionToTarget) < fieldOfView.angle / 2)
             {
                 // 내 위치와 target의 위치를 저장함
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                float distanceToTarget = Vector3.Distance(transform.position, wideTarget.position);
 
                 // 현재 위치에서 directionToTarget의 방향으로 distanceToTarget거리 만큼을 검사하며
                 // obstructionMask가 아니면 아마 targetMask이면 if문을 실행
-                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
-                    canSeePlayer = true; // 플레이어를 봄
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, fieldOfView.obstructionMask))
+                {
+                    agent.isStopped = false;
+                    fieldOfView.canSeePlayer = true;            // 플레이어를 봄
+                    animator.SetBool("canSeePlayer", true);     // animator 상태 변경
+                    agent.SetDestination(wideTarget.position);  // wideTarget 따라가기
+                }
                 else
-                    canSeePlayer = false; // 플레이어를 못 봄
+                    fieldOfView.canSeePlayer = false; // 플레이어를 못 봄
             }
             else
-                canSeePlayer = false; // 플레이어를 못 봄
+                fieldOfView.canSeePlayer = false; // 플레이어를 못 봄
         }
-        else if (canSeePlayer)
+        else if (fieldOfView.canSeePlayer)
         {
-            canSeePlayer = false; // 플레이어를 못 봄
+            fieldOfView.canSeePlayer = false;        // 플레이어를 못 봄
+            animator.SetBool("canSeePlayer", false); // animator 상태 변경
+            agent.isStopped = true;
         }
     }
-
-    protected abstract void Move();
 }
